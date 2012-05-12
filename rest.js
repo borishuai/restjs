af6e12b6379d30f7d8cac8dfae07d2dd1b9e3c41
borishuai <xbhuai@gmail.com>
2012-05-12 17:46:00 +0800

    the init code

--- rest.js
+++ rest.js
@@ -1,1 +1,8 @@
-//init version
+//     Rest.js 1.0
+//     Boris Huai Augmentum
+//     A utility lib of common function, which is supplementary to jQuery and underscorejs
+
+(function(window) {
+  var rest = {};
+  window.rest = rest;
+})(window);
