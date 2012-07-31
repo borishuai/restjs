f2658c0d002bed20eca672dba68aa4ac873e9147
boris <xbhuai@gmail.com>
2012-07-31 14:49:45 +0800

    add load script and style functions

--- rest.js
+++ rest.js
@@ -2,7 +2,56 @@
 //     Boris Huai Augmentum
 //     A utility lib of common function, which is supplementary to jQuery and underscorejs
 
-(function(window) {
+(function(window, document) {
   var rest = {};
+  
+  //load a javascript file into current file.
+  rest.loadScript = function(sURL) {
+    var dScript = document.createElement('script');
+    dScript.type = 'text/javascript';
+    dScript.src = sURL;
+    document.body.appendChild(dScript);
+  };
+  
+  //load inline script into current file.
+  rest.loadScriptString = function(sCode) {
+    var dScript = document.createElement('script');
+    dScript.type = 'text/javascript';
+    
+    try {
+      dScript.appendChild(document.createTextNode(sCode));
+    } catch (ex) {
+      dScript.text = sCode;
+    }
+
+    document.body.appendChild(dScript);
+  };
+  
+  //load a style file into current file
+  rest.loadStyle = function(sURL) {
+    var dLink = document.createElement('link');
+    var dHead = document.head || document.getElementsByTagName('head')[0];
+    
+    dLink.rel = 'stylesheet';
+    dLink.type = 'text/css';
+    dLink.href = sURL;
+    dHead.appendChild(dLink);
+  };
+  
+  rest.loadStyleString = function(sCSS) {
+    var dStyle = document.createElement('style');
+    var dHead = document.head || document.getElementsByTagName('head')[0];
+    
+    dStyle.type = 'text/css';
+    
+    try {
+      dStyle.appendChild(document.createTextNode(sCSS));
+    } catch (ex) {
+      dStyle.styleSheet.cssText = sCSS;
+    }
+    
+    dHead.appendChild(dStyle);
+  };
+  
   window.rest = rest;
-})(window);
+})(window, document);
